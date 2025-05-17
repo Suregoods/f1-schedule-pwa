2eaab9c3b0aa7d77c0dd07dd544773aaa8d934dc
Suregoods <jsmits@zekergoed.biz>
2025-05-17 12:59:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE_NAME = 'f1-schedule-cache-v2';
+const CACHE_NAME = 'f1-schedule-cache-v3';
 const assets = ['.', 'index.html', 'app.js', 'manifest.json'];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
